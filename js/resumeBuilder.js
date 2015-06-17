40dfff598f4f61a1528c388cdb0cd3ea0e2ec34e
Shawn S. Rajput <shawn.saeed01@gmail.com>
2015-06-16 20:14:25 -0500

    Resume shows work and project history added

---
+++ js/resumeBuilder.js
@@ -0,0 +1,272 @@
+var bio = {
+    "name" : "Shawn Rajput",
+    "role" : "Software Engineer",
+    "contacts" : [{
+        "mobile" : "540-628-5866",
+        "email" : "shawn.s.rajput@gmail.com",
+        "Github" : "rajputss", 
+        "location" : "Edmond, Oklahoma",
+        "Twitter" :"N/A"
+        
+    }], 
+    "welcomeMessage" : "A very ambitious technology professional who finds immense pleasure in software development and solving problems using innovative techniques.", 
+    "biopic" : "images/fry.jpg",
+    
+    "skills" : ["Motivated","Responsible","Love to teach","Avid Learner", "Energetic","Creative Thinker","Bilingual","Goal Oriented"]
+};
+
+var work = {
+  "jobs": [{
+    "employer": "LEIDOS, Inc.",
+    "title": "Software Engineer",
+    "location": "Oklahoma City, Oklahoma",
+    "dates": "January 2015 - Present",
+    "description": "LEIDOS contracts with the U.S. Airforce to update asset management system. My job involves converting inquire and update transaction from mainframe to Oracel Data Service."
+    },
+    {
+        "employer" : "Monsanto Company", 
+        "title" : "Software Developer",
+        "location" : "St. Louis, Missouri",
+        "dates" : "August 2014 - December 2014",
+        "description" : "Contributed to enhance GEOSPATIAL single page web application to provide end users with soil fertility information based on farm data entered by users."    
+    },
+    {
+        "employer" : "Principal Financial Group", 
+        "title" : "Software Developer",
+        "location" : "Des Moines, Iowa",
+        "dates" : "February 2014 - July 2014",
+        "description" : "Developed an internal web application for the fund managers who manage the portfolio. Users can set the client preferences and track the portfolio. It also provides the complete dashboard of the client."    
+    },
+    {
+        "employer" : "Freddie Mac", 
+        "title" : "Software Developer",
+        "location" : "Mclean, Virginia",
+        "dates" : "July 2013 - January 2013",
+        "description" : "Developed a web application for the funding department of Freddie Mac to monitor the cash flow. It reports the share of mortgage securities and validates the liquidity of the assets." 
+    },
+    {
+        "employer" : "GEICO", 
+        "title" : "Financial Data Analyst",
+        "location" : "Fredericksburg, Virginia",
+        "dates" : "January 2010 - June 2013",
+        "description" : "Interacted with businesses and functional owners from different departments to engineer and conduct high-level review sessions to derive and execute action plans, meeting deadlines and standards."    
+    },
+    {
+        "employer" : "GEICO", 
+        "title" : "Business Expense Analyst",
+        "location" : "Virginia Beach, Virginia",
+        "dates" : "June 2006 - December 2009",
+        "description" : "Prepared, reviewed, justified and presented departmental budget as the principal analyst in formulating, presenting, reviewing, justifying, and executing program budgets."    
+    },
+    {
+        "employer" : "GEICO", 
+        "title" : "Sales Counselor",
+        "location" : "Virginia Beach, Virginia",
+        "dates" : "May 2003 - May 2006",
+        "description" : "Selling and servicing in bound calls of potential policy holder. Binding contract, accepting payments, servicing accounts, explaining the terms and conditions of the policies."    
+    }]
+};
+
+var education = {
+	"schools" : [
+	{
+	"name" : "Old Dominion University",
+	"city" : "Norfolk",
+	"degree" : "Bachelors",
+	"majors" : ["Finance"],
+	"dates" : 2009,
+	"url" : "http://odu.edu"
+}],
+
+	"onlineCourses": [{
+    "title": "Intro to Computer Science",
+    "school": "Udacity",
+    "dates": "January 2015 - March 2015",
+    "url": "https://www.udacity.com/course/cs101"
+  }, {
+    "title": "Intro to Java Programming",
+    "school": "Udacity",
+    "dates": "October 2014 - December 2014",
+    "url": "https://www.udacity.com/course/cs046"
+  }, {
+    "title": "Programming Foundations with Python",
+    "school": "Udacity",
+    "dates": "February 2015 - February 2015",
+    "url": "https://www.udacity.com/course/ud036"
+  }, {
+    "title": "Intro to jQuery",
+    "school": "Udacity",
+    "dates": "June 2015 - June 2015",
+    "url": "https://www.udacity.com/course/cs245"
+  }, {
+    "title": "Intro to HTML and CSS",
+    "school": "Udacity",
+    "dates": "February 2015 - February 2015",
+    "url": "https://www.udacity.com/course/ud304"
+  }, {
+    "title": "Python",
+    "school": "Codecademy",
+    "dates": "January 2015 - January 2015",
+    "url": "http://www.codecademy.com/en/tracks/javascript"
+  }, {
+    "title": "JavaScript Basics",
+    "school": "Udacity",
+    "dates": "June 2015 - June 2015",
+    "url": "https://www.udacity.com/course/ud804"
+  }, {
+    "title": "Full Stack Foundations",
+    "school": "Udacity",
+    "dates": "February 2015 - March 2015",
+    "url": "https://www.udacity.com/course/ud088"
+  }, {
+    "title": "Authentication & Authorization: OAuth",
+    "school": "Udacity",
+    "dates": "March 2015 - April 2015",
+    "url": "https://www.udacity.com/course/ud330"
+  }, {
+    "title": "Intro to AJAX",
+    "school": "Udacity",
+    "dates": "May 2015 - June 2015",
+    "url": "https://www.udacity.com/course/ud110"
+  }, {
+    "title": "Intro to Algorithms",
+    "school": "Udacity",
+    "dates": "July 2015 - July 2015",
+    "url": "https://www.udacity.com/course/cs215"
+  }, {
+    "title": "How to Use Git and GitHub",
+    "school": "Udacity",
+    "dates": "June 2015 - June 2015",
+    "url": "https://www.udacity.com/course/ud775"
+	}
+	]
+};
+
+var projects = {
+  "project": [{
+    "title": "Item Catalog",
+    "url": "https://github.com/rajputss/Project3_Item_Catalog",
+    "dates": "May 2015",
+    "description": "This web application was the third project for full stack Udacity nanodegree that provides a list of items within a variety of categories as well as provide a user registration and authentication system. Registered users will have the ability to post, edit and delete their own items.",
+    //"image": "images/dw_wiki_s_mini.jpg"
+  }, {
+    "title": "Conference Central",
+    "url": "https://github.com/rajputss/Project4_Conference_Organization_App",
+    "dates": "July 2015",
+    "description": "This web application was the fourth project for full stack Udacity nanodegree (Developing Scalable Apps with Python). The project was built on Google App Engine.",
+    //"image": "images/dw_scalable_s_mini.jpg"
+  }, {
+    "title": "Movie Trailer Website",
+    "url": "https://github.com/rajputss/Project-1-Movie-Trailer-Website-",
+    "dates": "February 2015",
+    "description": "This is the first project for full stack Udacity nanodegree that has server-side code to store a list favorite movies, including box art imagery and a movie trailer URL. This data will be served as a web page allowing visitors to review their movies and watch the trailers.",
+    //"image": "images/dw_portfolio_s.jpg"
+  }, {
+    "title": "Relational Database Tournament",
+    "url": "https://github.com/rajputss/Project2_RelationalDatabase_Tournament",
+    "dates": "March 2015",
+    "description": "This is the second project for full stack Udacity nanodegree, where a database schema was built to store the game matches between players. Code was written to query this data and determine the winners of various games.",
+    //"image": "images/dw_resume_s.png"
+  }]
+};
+
+bio.display = function() {
+    var formattedName = HTMLheaderName.replace("%data%",bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+    var formattedImage = HTMLbioPic.replace("%data%",bio.biopic);
+    var formattedMessage = HTMLWelcomeMsg.replace("%data%",bio.welcomeMessage);
+    $("#header").prepend(formattedRole).prepend(formattedName).append(formattedImage,formattedMessage);
+    $("#header").append(HTMLskillsStart);
+
+    for(skill in bio.skills) {
+        var formattedSkills = HTMLskills.replace("%data%",bio.skills[skill]);
+        $("#skills").append(formattedSkills);
+    };
+
+    for(contact in bio.contacts) {
+        var formattedMobile = HTMLmobile.replace("%data%",bio.contacts[contact].mobile);
+        var formattedEmail = HTMLemail.replace("%data%",bio.contacts[contact].email);
+        var formattedGithub = HTMLgithub.replace("%contact%","Github").replace("%data%",bio.contacts[contact].Github);
+        var formattedLocation =HTMLlocation.replace("%data%", bio.contacts[contact].location);
+        var formattedTwitter =HTMLtwitter.replace("%data%", bio.contacts[contact].Twitter);
+        $("#footerContacts").append(formattedMobile,formattedEmail,formattedGithub,formattedTwitter,formattedLocation);
+        $("#topContacts").append(formattedMobile,formattedEmail,formattedGithub,formattedTwitter,formattedLocation);
+    };
+};
+
+education.display = function() {
+        
+    for(school in education.schools) {
+        $("#education").append(HTMLschoolStart);
+        
+        var formattedName = HTMLschoolName.replace("%data%",education.schools[school].name);
+        var formattedDegree = HTMLschoolDegree.replace("%data%",education.schools[school].degree);
+        var formattedDates = HTMLschoolDates.replace("%data%",education.schools[school].dates);
+        var formattedLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
+        var formattedMajor = HTMLschoolMajor.replace("%data%",education.schools[school].majors);
+        $(".education-entry:last").append(formattedName + formattedDegree,formattedDates,formattedLocation,formattedMajor);
+    }
+
+ $("#education").append(HTMLonlineClasses);
+          for (var course in education.onlineCourses) {
+             
+              $("#education").append(HTMLschoolStart);
+                  var formattedonlineTitle =HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+                  var formattedonlineSchool =HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+                  var formattedonlineDate =
+                  HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+                  var formattedURL =HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+                 $(".education-entry:last").append(formattedonlineTitle + formattedonlineSchool,formattedonlineDate,formattedURL);
+                }
+};
+
+work.display = function() {
+    for(job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+        
+        var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+        var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+        var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+
+        $(".work-entry:last").append(formattedEmployer + formattedTitle,formattedDates,formattedDescription);
+    }
+};
+
+projects.display = function(){
+    for(item in projects.project){
+        $("#projects").append(HTMLprojectStart);
+        var formattedTitle = HTMLprojectTitle.replace("%data%",projects.project[item].title);
+        var formattedDates = HTMLprojectDates.replace("%data%",projects.project[item].dates);
+        var formattedDescription = HTMLprojectDescription.replace("%data%",projects.project[item].description);
+        
+        $(".project-entry:last").append(formattedTitle,formattedDates,formattedDescription);
+        for (image in projects.project[item].images) {
+            var formattedImage = HTMLprojectImage.replace("%data%",projects.project[item].images[image]);
+            $(".project-entry:last").append(formattedImage);
+        };
+        
+
+        
+    }
+};
+
+function inName(name){
+    console.log(name);
+    var newName = name;
+    newName = newName[0].toUpperCase() + newName.slice(1,newName.indexOf(" ") + 1).toLowerCase() + newName.slice(newName.indexOf(" ") + 1).toUpperCase(); 
+
+    return newName;
+};
+
+bio.display();
+work.display();
+projects.display();
+education.display();
+
+
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap);
+
+
+
